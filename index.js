d666d95f4cd965fd1bd113ba0549112714660b6f
0m3ga13 <barkaoussama13@gmail.com>
2023-07-28 21:43:46 +0100

    done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const returnFirstTwoDrivers = (drivers) => drivers.slice(0, 2);
 const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
 let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
-const createFareMultiplier = (multiplier) => fareMultiplier = (fare) => fare*multiplier;
+const createFareMultiplier = a => fare => fare * a;
 const fareDoubler =  createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 const selectDifferentDrivers = (arrayOfDrivers, fn) =>  fn(arrayOfDrivers);
+
+console.log(fareDoubler(15))
